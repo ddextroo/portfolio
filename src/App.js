ef3987f7218036a4506fac068be409fb4dca335b
ddextroo <dexteringuito@gmail.com>
2023-11-06 10:52:16 +0800

    done

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Confetti from "react-confetti";
 import ExperienceItem from "./components/experience";
+import Marquee from "react-fast-marquee";
 
 function App() {
   // https://github.com/developedbyed/react-portofolio-with-tailwind/blob/main/pages/index.js
@@ -461,6 +462,7 @@ function App() {
                   knowledge even further and strive for excellence in my future
                   projects.
                 </h3>
+
                 <audio className="w-full mt-5" controls>
                   <source src={audio} type="audio/wav" />
                   Your browser does not support the audio tag.
@@ -478,23 +480,101 @@ function App() {
                 </div> */}
               </div>
             </div>
+            <h2 className=" text-4xl text-colorAccent font-bold md:text-3xl md:mt-10 sm:mt-5">
+              Gallery
+            </h2>
+            <Marquee
+              gradient={false}
+              gradientColor={[100, 100, 100]}
+              speed={40}
+              pauseOnHover={false}
+              style={{
+                borderRadius: "10px",
+              }}
+            >
+              <div className="flex flex-row md: mt-10">
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2Fsketch1588494312824.jpg?alt=media&token=be54e4a9-dbaa-45d7-b64c-0c0a75220fb5&_gl=1*14dh3o3*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzYzNTguNDcuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2F1582079745245.jpg?alt=media&token=eb7d2e4e-074b-4d86-ac9d-447b64913d41&_gl=1*ivb3zr*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY1MjEuNDguMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2F1588230595317.jpg?alt=media&token=269769d7-b307-46ab-b68e-a74801299d26&_gl=1*z7xj7j*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY1MzguMzEuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2F1589878996304_95193523.png?alt=media&token=1f422a63-c126-4801-9adc-9c676957d66b&_gl=1*ic921r*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY1NTguMTEuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2F1590068882874.jpg?alt=media&token=777f009d-70eb-41f2-a3bc-77ef309c4ccc&_gl=1*hmp1w3*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY2MjYuNjAuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2F1590673592629_393820671.png?alt=media&token=2ddf7005-2e75-4bbc-89a7-58239d2381eb&_gl=1*atq3b4*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY2NDIuNDQuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2F715cefb1-442f-4fea-a8bb-6b3a5ec35bc9.png?alt=media&token=d773d64b-2d09-4473-8c4e-df6cd4cfb679&_gl=1*10b7g1p*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY2NTMuMzMuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2F79908695-7391-4e5a-99ec-59fa31b2675d.png?alt=media&token=77bdf957-f8ef-4ae0-a764-49f30bf501e1&_gl=1*o83kso*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY2NzIuMTQuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2F7c970f14-1d9c-4220-b7bd-736ed1ec3e05.png?alt=media&token=eb69986c-ae24-4181-882e-6f6f6c71ee20&_gl=1*19h2dbp*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY2ODUuMS4wLjA."
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2FIMG_20201224_192725.jpg?alt=media&token=2646b481-ae3b-479f-8880-351c5f6154c5&_gl=1*qa3eqh*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY3MzUuMjEuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2FIMG_20210522_103815.jpg?alt=media&token=55320436-033d-460e-bd62-abb9e479b967&_gl=1*72qws9*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY3NTAuNi4wLjA."
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2FLANDSCAPE_Light%20trails.jpg?alt=media&token=bd63c7b0-8ff6-44ff-b224-d9c3f4c410ff&_gl=1*dhzpls*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY3ODQuNDUuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2FLRM_EXPORT_14204989112132_20191230_001255813.jpeg?alt=media&token=402d56fc-7e3f-4170-81ff-5a439e3818f1&_gl=1*98klo9*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY4NDIuNjAuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2FLRM_EXPORT_28472159103096_20191105_140217355.jpeg?alt=media&token=67e4a816-4789-4bb8-aafc-bc2a2c24d983&_gl=1*1lpxkiv*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY4NTcuNDUuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/ddev-77971.appspot.com/o/post%2FLRM_EXPORT_379985255874837_20200108_180826337.jpeg?alt=media&token=50229a4d-4d09-4a48-9313-d344b8f874f6&_gl=1*8b5nby*_ga*MTY5MTQ2MC4xNjk3Njk0MDU5*_ga_CW55HF8NVT*MTY5OTIzNjIzMC4yNy4xLjE2OTkyMzY4NzYuMjYuMC4w"
+                  className="w-fit h-44 rounded-lg mx-5"
+                ></img>
+              </div>
+            </Marquee>
           </div>
         </section>
         <section
           id="experience"
           className="experience p-10 min-h-fit font-poppins"
         >
-          <h2 className=" text-4xl text-colorAccent font-bold md:text-3xl py-2">
-            Experience
-          </h2>
-          <p className="font-poppins text-md text-gray">
-            My experience in tech
-          </p>
+          <div className="ml-14">
+            <h2 className=" text-4xl text-colorAccent font-bold md:text-3xl py-2">
+              Experience
+            </h2>
+            <p className="font-poppins text-md text-gray">
+              My experience in tech
+            </p>
+          </div>
           <div className="max-w-xl mx-auto">
             <ExperienceItem
               year="2023-2024"
               title="Google Developer Student Club"
               description="Mobile Development Lead"
+              isFirst
             />
             <ExperienceItem
               year="2021-Present"
@@ -514,7 +594,8 @@ function App() {
             <ExperienceItem
               year="2018-2020"
               title="Security Researcher"
-              description="Self taught cybersecurity stuff"
+              description="Self taught on cybersecurity stuff"
+              isLast
             />
             {/* Add more ExperienceItem components for additional experiences */}
           </div>
@@ -606,7 +687,9 @@ function App() {
           </div>
         </section>
         <footer className="backdrop-filter backdrop-blur-lg rounded-t-xl border-white/30 border bg-primaryDarkLight/30 text-gray py-4 text-center font-poppins">
-          <p>Copyright &copy; 2019 - {currentYear} DDev | All Rights Reserved.</p>
+          <p>
+            Copyright &copy; 2019 - {currentYear} DDev | All Rights Reserved.
+          </p>
         </footer>
         {showModal && <Modal onClose={closeModal} />}
       </div>
--- src/components/experience.js
+++ src/components/experience.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-const ExperienceItem = ({ year, title, description }) => (
+const ExperienceItem = ({ year, title, description, isFirst, isLast}) => (
   <div className="relative p-6">
+    {/* Timeline Line */}
+    <div className={`w-2 ml-3 bg-colorAccent absolute top-0 bottom-0 ${isFirst ? 'rounded-t-lg' : ''} ${isLast ? 'rounded-b-lg' : ''}`}></div>
     {/* Timeline Dot */}
     <div className="absolute h-8 w-8 bg-colorAccent rounded-full border-4 border-white"></div>
-
-    {/* Timeline Line */}
-    <div className="w-2 bg-colorAccent rounded-lg absolute top-0 bottom-0 mt-4"></div>
-
     {/* Timeline Content */}
     <div className="ml-12 p-4 border-l-4 backdrop-filter backdrop-blur-lg rounded-xl border-white/30 border bg-primaryDarkLight/30 shadow-md">
       <h3 className="text-2xl font-semibold mb-2 text-gray">{year}</h3>
